73b40069bc7ac41af9de18d717d4890d483d1422
Jesus david <chuchober@hotmail.com>
2022-06-16 13:50:07 -0500

    Add API Routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PetpostController;
-use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\PetpostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,11 @@ Route::apiResource('v1/petpost', PetpostController::class)
 
 Route::apiResource('v1/user', UserController::class)
     ->only(['index']);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::get('me', 'me');
+});
